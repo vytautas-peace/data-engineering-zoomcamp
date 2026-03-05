5b2b9cd06c826d6721264c6aec94a824b480274d
vytautas-peace <vytautas.peace@gmail.com>
2026-03-05 10:41:55 +0000

    terraform pt.1

--- 02-terraform/main.tf
+++ 02-terraform/main.tf
@@ -9,5 +9,21 @@ terraform {
 
 provider "google" {
   project = "terraform-489308"
-  region = "us-central1"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name                        = "terraform-489308-demo-bucket"
+  location                    = "US"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
